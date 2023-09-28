60d37e15bcfc6fa2f696dc5bfe1e52b9960e000c
MarlonBair <marlonbair1@gmail.com>
2023-09-27 21:16:53 -0700

    Initial commit

---
+++ src/test/java/com/hobbyshop/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hobbyshop.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
